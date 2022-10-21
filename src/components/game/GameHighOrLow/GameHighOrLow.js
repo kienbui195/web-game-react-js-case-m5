9e1d3ed8cc62aa30ef21bf25650efe916a315fe9
Kay Bi <kaybi9x@gmail.com>
2022-10-21 17:07:34 +0700

    update game

--- src/components/game/GameHighOrLow/GameHighOrLow.js
+++ src/components/game/GameHighOrLow/GameHighOrLow.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react-hooks/rules-of-hooks */
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
@@ -6,7 +7,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 // eslint-disable-next-line import/no-unresolved
@@ -16,107 +17,117 @@ import { getValue } from '../../../features/gameSuggestSlice';
 const randomNum = () => Math.floor(Math.random() * 11);
 
 const GameHighOrLow = () => {
-    const mid = 5;
-	const navigate = useNavigate();
-	const [second, setSecond] = useState();
-	const [result, setResult] = useState(0);
-	const dispatch = useDispatch();
-	const [mess, setMess] = useState('')
-	const userSelect = useSelector(state => state.dataGame.value);
+  const mid = 5;
+  const navigate = useNavigate();
+  const [second, setSecond] = useState();
+  const [result, setResult] = useState(0);
+  const dispatch = useDispatch();
+  const [mess, setMess] = useState('');
+  const userSelect = useSelector((state) => state.dataGame.value);
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		dispatch(getValue(e.target.data.value));
-		setSecond(randomNum());
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!e.target.data.value) {
+      setMess('Bạn cần lựa chọn trước khi đoán!');
+    } else {
+      dispatch(getValue(e.target.data.value));
+      setSecond(randomNum());
+    }
+  };
 
-	const userInfo = {
-		email: localStorage.getItem('email'),
-		code: localStorage.getItem('code')
-	}
+  const userInfo = {
+    email: localStorage.getItem('email'),
+    code: localStorage.getItem('code'),
+  };
 
-	const updatePoint = async () => {
-		const result =  await axios.request({
-			url: 'https://webgame395group.herokuapp.com/api/setpoint',
-			method: 'POST',
-			Headers: { 'Content-Type': 'Application/json' },
-			data: JSON.stringify({
-				email: `${userInfo.email}`,
-				code: `${userInfo.code}`
-			}),
-        });
-        
-        return result
-	};
+  const updatePoint = async () => {
+    const result = await axios.request({
+      url: 'https://webgame395group.herokuapp.com/api/setpoint',
+      method: 'POST',
+      Headers: { 'Content-Type': 'Application/json' },
+      data: JSON.stringify({
+        email: `${userInfo.email}`,
+        code: `${userInfo.code}`,
+      }),
+    });
 
-	const handleExit = () => {
-		updatePoint()
-			.then(res => {
-				if (res.data.type === 'success') {
-					navigate('/');
-				} else {
-					setMess('Bạn cần phải login mới lưu được điểm số!')
-					setTimeout(()=>navigate('/login'),2000)
-				}
-			})
-			.catch(err => console.log(err.message));
-	};
+    return result;
+  };
 
-	useEffect(() => {
-		console.log(userSelect);
-		if (second) {
-			if (second > mid) {
-				if (userSelect === 1) {
-					setResult(result + 2);
-				} else {
-					setResult(result - 1);
-				}
-			} else if (userSelect === 1) {
-					setResult(result - 1);
-				} else {
-					setResult(result + 2);
-				}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [second]);
+  const handleExit = () => {
+    updatePoint()
+      .then((res) => {
+        if (res.data.type === 'success') {
+          navigate('/');
+        } else {
+          setMess('Bạn cần phải login mới lưu được điểm số!');
+          setTimeout(() => navigate('/login'), 2000);
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-	return (
-		<div>
-			<Grid>
-				<Grid item xs />
-				<Grid item xs={8} sx={{ textAlign: 'center' }}>
-					<Paper elevation={3} sx={{ padding: 2, marginTop: 10, textAlign: 'center', maxWidth: 300 , marginLeft: 26}}>
-						{mess ? (<Alert severity='warning'>{mess}</Alert>) : ''}
-						<h2> Higher Or Lower Game???</h2>
-						<p>Your Point: {result || 0}</p>
+  useEffect(() => {
+    console.log(userSelect);
+    if (second) {
+      if (second > mid) {
+        if (userSelect === 1) {
+          setResult(result + 2);
+        } else {
+          setResult(result - 1);
+        }
+      } else if (userSelect === 1) {
+        setResult(result - 1);
+      } else {
+        setResult(result + 2);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [second]);
 
-						<Box component='form' onSubmit={handleSubmit}>
-							<TextField
-								value={second || 5}
-								disabled
-								type='number'
-								inputProps={{ style: { textAlign: 'center' } }}
-							/>
-							<div>
-								<input type='radio' value={1} name='data' />
-								Higher
-								<input type='radio' value={-1} name='data' />
-								Lower
-							</div>
+  return (
+    <div>
+      <Grid>
+        <Grid item xs />
+        <Grid item xs={8} sx={{ textAlign: 'center' }}>
+          <Paper
+            elevation={3}
+            sx={{ padding: 2, marginTop: 3, textAlign: 'center', maxWidth: 350, marginLeft: 26 }}
+            variant="contained"
+          >
+            {mess ? <Alert severity="warning">{mess}</Alert> : ''}
+            <h2>
+              <span style={{ color: 'red' }}>Cao hơn</span> Hay <span style={{ color: 'green' }}>Thấp hơn</span>??
+            </h2>
+            <p style={{ color: 'blue' }}>Điểm của bạn: {result || 0}</p>
 
-							<Button type='submit' variant='contained' color='success' sx={{ marginTop: 3 }}>
-								Guess
-							</Button>
-						</Box>
-						<Button type='button' onClick={handleExit} variant='outlined' sx={{ marginTop: 1 }}>
-							Save Point
-						</Button>
-					</Paper>
-				</Grid>
-				<Grid item xs/>
-			</Grid>
-		</div>
-	);
+            <Box component="form" onSubmit={handleSubmit}>
+              <p style={{ fontSize: 10 }}>Số hiện tai</p>
+              <p style={{ fontSize: 60, color: 'red', margin: 0 }}>{second || 5}</p>
+              <div>
+                <label>
+                  <input type="radio" value={1} name="data" />
+                  Cao hơn
+                </label>
+                <label>
+                  <input type="radio" value={-1} name="data" />
+                  Thấp hơn
+                </label>
+              </div>
+
+              <Button type="submit" variant="contained" color="success" sx={{ marginTop: 3 }}>
+                Guess
+              </Button>
+            </Box>
+            <Button type="button" onClick={handleExit} variant="outlined" sx={{ marginTop: 1 }}>
+              Save Point
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs />
+      </Grid>
+    </div>
+  );
 };
 
 export default GameHighOrLow;
