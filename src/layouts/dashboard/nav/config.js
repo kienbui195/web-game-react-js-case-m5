6fc7ee7d37f9569927c2fc998ba93a81f6ec2864
Your Name <you@example.com>
2022-10-23 15:57:54 +0700

    leaderboard

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -8,7 +8,7 @@ const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ 
 const navConfig = [
   {
     title: 'Leaderboard',
-    path: '/dashboard/app',
+    path: '/dashboard/leaderboard',
     icon: icon('ic_analytics'),
   },
   {
