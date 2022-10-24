2d6bd90392f7d462cec93f0ee358dc788db7196d
Your Name <you@example.com>
2022-10-24 10:16:02 +0700

    user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,25 +1,23 @@
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {CardActionArea, Grid} from '@mui/material';
 
 const UserProfile = () => {
-  const [userInfo, setUserInfo] = useState({
-    email: '',
-    username: '',
-    password: '',
-  });
-  const [status, setStatus] = useState(false);
+  const [user, setUser] = useState({})
 
   const getUserInfoAPI = async () => {
+    const user = JSON.parse(localStorage.getItem('user'))
     const result = await axios.request({
       url: 'https://webgame395group.herokuapp.com/api/user/info',
       method: 'POST',
       headers: { 'Content-Type': 'Application/json' },
       data: JSON.stringify({
-        email: localStorage.getItem('email'),
-        code: localStorage.getItem('code'),
+        email: user.email,
+        code: user.code,
       }),
     });
     return result;
@@ -30,29 +28,41 @@ const UserProfile = () => {
       .then((res) => {
         if (res.data.type === 'success') {
           const user = res.data.message;
-          setUserInfo({
-            email: user.email,
-            username: user.username,
-            password: user.password,
-          });
+          setUser(user)
         } else console.log(res.data.message);
       })
       .catch((err) => console.log(err.message));
   });
 
   return (
-    <div>
-      <Grid container spacing={3}>
+      <Grid container>
         <Grid item xs />
-        <Grid item xs>
-          <Paper elevation={2} xs={{ padding: 2 }}>
-                      <h2>Hello {userInfo.username}</h2>
-                      <TextField    />
-          </Paper>
+        <Grid item xs sx={{marginTop:10}}>
+          <h2>Your Profile</h2>
+          <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+                component="img"
+                height="350"
+                image="https://image-us.24h.com.vn/upload/1-2022/images/2022-03-16/baukrysie_275278910_3174792849424333_1380029197326773703_n-1647427653-670-width1440height1800.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {user.username}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Email: {user.email}
+                <br />
+                Point: {user.point}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
         </Grid>
         <Grid item xs />
       </Grid>
-    </div>
+
   );
 };
 
