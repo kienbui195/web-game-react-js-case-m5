316510ec952e8bbd32fab3aa97a822a0bb35a4f2
Your Name <you@example.com>
2022-10-24 11:13:21 +0700

    sualoi

--- src/components/game/RockPaperScissors/GameResultsView/styledComponents.js
+++ src/components/game/RockPaperScissors/GameResultsView/styledComponents.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const GameViewContainer = styled.div`
-  display: flex
+  display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
