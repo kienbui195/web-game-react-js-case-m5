504567c2ecf230f832b53fc577b3a33196258f53
Your Name <you@example.com>
2022-10-25 14:19:48 +0700

    upabcd

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -196,7 +196,7 @@ export default function DashboardAppPage() {
                 </CardContent>
                 <CardActions>
                   <Button size="small">Share</Button>
-                  <Button onClick={handleSendUser} size="small" href="http://localhost:3001/2048">
+                  <Button onClick={handleSendUser} size="small" href="https://monicatvera.github.io/2048/">
                     Play Game
                   </Button>
                 </CardActions>
